e3f3cad435b4c03d8150986e196702b66868ae95
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-02-17 16:51:53 +0100

    Add files via upload

---
+++ Benvido.py
@@ -0,0 +1,15 @@
+print("Boas Tardes")
+
+nome=input("Como te chamas?")
+print("O teu nome é", nome)
+
+idade=input ("Cantos anos tes?")
+print("Que madurito estás" , idade, "anos, que mogollón" )
+
+print("Feliz día")
+
+print("Agora matemáticas")
+
+numerito = int(input("Introduce un número"))
+
+print ("O dobre de teu número é", numerito*2 )

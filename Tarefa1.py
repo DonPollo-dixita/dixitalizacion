acb24f498a2596e94b1edc6323500fe1b3a9e82f
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-02-18 13:46:08 +0100

    Add files via upload

---
+++ Tarefa1.py
@@ -0,0 +1,13 @@
+print("Moi bos días a todos")
+
+print("Agora non vai facer a conta so vai decir exactamente o que hai entre as comillas =", "2+2")
+
+print("Agora vai facer a conta =", 2+2)
+
+print("Agora non vai facer a conta so vai decir exactamente o que hai entre as comillas =", "8+10")
+
+print("Agora vai facer a conta =", 8+10)
+
+print("Agora non vai facer a conta so vai decir exactamente o que hai entre as comillas =", "3*2")
+
+print("Agora vai facer a conta =", 3*2)

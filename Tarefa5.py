5721a2684e0d4ca0731d9861616f205e9e784b0b
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-02-24 16:48:28 +0100

    Add files via upload

---
+++ Tarefa5.py
@@ -0,0 +1,49 @@
+print("**********************")
+
+distancia = input("Cantos km fas ao día?  ")
+
+distanciaNum = int(distancia)
+
+metros = 1000*distanciaNum
+
+print("Iso son", metros, "metros")
+
+print("**********************")
+
+
+print("**********************")
+
+resposta = input("Cantas ducias de ovos queres?  ")
+
+respostaNum = int(resposta)
+
+ovos = 12*respostaNum
+
+print("Tes que comprar", ovos, "este número de ovos")
+
+print("**********************")
+
+
+print("**********************")
+
+dinero = input("Cantos euros tes?  ")
+
+dineroNum = int(dinero)
+
+euros = 43*dineroNum
+
+print("Iso son", euros, "grivnas")
+
+print("**********************")
+
+print("**********************")
+
+cartos = input("Cantos euros tes?  ")
+
+cartosNum = int(cartos)
+
+eur = 1.05*cartosNum
+
+print("Iso son", eur, "dólares")
+
+print("**********************")

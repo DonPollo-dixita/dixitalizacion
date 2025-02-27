223cf18b4e28190bfcff75fd58f37f7cc591d8ce
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-02-27 13:54:26 +0100

    Add files via upload

---
+++ caixarapida.py
@@ -0,0 +1,37 @@
+print("**********************")
+
+print("===Don-Pollo-Dixita===")
+
+print("----------------------")
+
+primero = input("1º producto ")
+
+primeroNum = int(primero)
+
+print(" ")
+
+segundo = input("2º producto ")
+
+segundoNum = int(segundo)
+
+print(" ")
+
+tercer = input("3º producto ")
+
+tercerNum = int(tercer)
+
+total = primeroNum + segundoNum + tercerNum
+
+print("----------------------")
+
+print("Total a pagar", total,"€")
+
+print("----------------------")
+
+print(" ")
+
+print("= = = = Bo-Día = = = =")
+
+print(" ")
+
+print("**********************")

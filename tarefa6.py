4585cb70ec25047946e2532aca8e030033118744
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-03-06 13:59:26 +0100

    Add files via upload

---
+++ tarefa6.py
@@ -0,0 +1,21 @@
+persoa = input ("Como te chamas? ")
+
+if(persoa == "Iván"):
+    print("Bos días", persoa)
+elif (persoa =="Marcos"):
+    print("Traballa un chisco")
+elif (persoa =="Martín"):
+    print("Deixa o ARK")
+elif (persoa =="Pita"):
+    print("Deixa o vaper")
+else:
+    print("Non sei quen eres")
+
+print(" ")
+
+número = input ("Cal é o teu número favorito? ")
+
+if(número == "7"):
+    print("Que casualidade o",número,"tamén é o número favorito da profe")
+else:
+    print("O meu é o 20")

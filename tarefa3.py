d7d96e680a7f5cfb647802d9978c0bcec22c88c9
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-03-10 16:29:41 +0100

    Add files via upload

---
+++ tarefa3.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.sqrt(33))
+
+Num1 = 123.456
+Num2 = 654.321
+Num3 = 456.123
+
+print(math.sqrt(Num1))
+print(math.pow(Num2,2))
+
+print(math.pi)
+print(round(math.pi))
+
+print(min(Num1,Num2,Num3))
+import random
+print(random.randint(0,3))

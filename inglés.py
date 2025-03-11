f38158524f9cc04013183409d8efd273c6e5100f
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-03-11 13:58:41 +0100

    Add files via upload

---
+++ inglés.py
@@ -0,0 +1,32 @@
+house = input("Como se dice CASA en inglés")
+
+if(house == "house")
+    print("Moi ben")
+
+print = ("")
+
+dog = input("Como se dice PERRO en inglés")
+
+if(dog == "dog")
+    print("Moi ben")
+
+print = ("")
+
+cat = input("Como se dice GATO en inglés")
+
+if(cat == "cat")
+    print("Moi ben")
+
+print = ("")
+
+dinner = input("Como se dice CENA en inglés")
+
+if(dinner == "dinner")
+    print("Moi ben")
+
+print = ("")
+
+mouse = input("Como se dice RATÓN en inglés")
+
+if(mouse == "mouse")
+    print("Moi ben")

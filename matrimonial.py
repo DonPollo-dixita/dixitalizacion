86170769d92b4c3cdd73d0c0c1b2bed7d4ea8fcb
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-03-17 16:47:25 +0100

    Add files via upload

---
+++ matrimonial.py
@@ -0,0 +1,22 @@
+print("Benvido á busca da parella ideal")
+
+ojos = input ("Cal é a cor de ollos favorito? ")
+
+if(ojos == "Negro"):
+    print("A tua media laranxa é Neymar Jr.")
+elif (ojos == "Marrón"):
+    print("A tua media laranxa é Fernando Alonso")
+
+país = input ("Cal é o país da túa vida? ")
+
+if(país == "Brasil"):
+    print("A tua media laranxa é Neymar Jr.")
+elif (país == "España"):
+    print("A tua media laranxa é Fernando Alonso")
+
+deporte = input ("Cal é o teu deporte preferido? ")
+
+if(deporte == "Fútbol"):
+    print("A tua media laranxa é Neymar Jr.")
+elif (deporte == "Piloto"):
+    print("A tua media laranxa é Fernando Alonso")

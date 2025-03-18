f594aeb2e20bbcc3941378c71a537deadacc7a09
DonPollo-dixita <ivanpereirainstituto@gmail.com>
2025-03-18 13:58:31 +0100

    Add files via upload

---
+++ famosetes.py
@@ -0,0 +1,24 @@
+print("...")
+
+edad = input ("Cantos anos ten o teu personaxe? ")
+
+país = input ("Cal é o país do teu famoso? ")
+
+deporte = input ("Cal é o teu deporte do teu personaxe? ")
+
+
+if(país == "Angola"):
+    elif(edad == "25"):
+    elif(deporte == "Fútbol"):
+    print("O teu personaxe é Afimico Pululu")
+
+if(país == "EEUU"):
+    elif(edad == "30"):
+    elif(deporte == "Baloncesto"):
+    print("O teu personaxe é LeBron James")
+
+
+if(país == "España"):
+    elif(edad == "22"):
+    elif(deporte == "Fútbol"):
+    print("O teu personaxe é Pedri")
